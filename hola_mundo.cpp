fc34fbbad1a9d3368de58ce4f2c548fb7be2cfc3
diego78 <diegonzalezven@gmail.com>
2025-09-26 10:02:08 -0400

    feat: crear programa hola mundo inicial

---
+++ hola_mundo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "¡Hola Mundo desde Git!" << endl;
+    cout << "Mi nombre es: [Tu Nombre]" << endl;
+    return 0;
+}//
+// Created by Diegoo on 9/26/2025.
+//
